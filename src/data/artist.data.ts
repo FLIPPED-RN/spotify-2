45adc650a153de15f99c4002fc75b81434cb3650
FLIPPED-RN <rnazarov142@gmail.com>
2026-02-19 23:59:53 +0200

    v0.0.1 | start

---
+++ src/data/artist.data.ts
@@ -0,0 +1,10 @@
+import type { IArtist } from "../types/artist.types";
+
+export const ARTISTS: IArtist[] = [
+	{
+		name: "FLIPPER",
+		image: '',
+		listenersCount: 1241253,
+		tracks: []
+	}
+]
---
+++ src/data/tracks.data.ts
@@ -0,0 +1,11 @@
+import type { ITrack } from "../types/track.types";
+import { ARTISTS } from "./artist.data";
+
+export const TRACKS: ITrack[] = [
+  {
+    name: "Розовое вино (FLIPPER EDIT)",
+    file: "/audio/FLIPPER - Розовое вино.mp3",
+    artist: ARTISTS[0],
+    duration: 114, //1:54
+  },
+];
